f5db10a28f59a2f9550383536e8ca024ffe0a9dc
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-01-11 18:10:36 -0800

    refactoring.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
-exports.bunyan = function(name, enableFileOutput) {
-    const Logger = require('./lib/bunyan');
+exports.setup = function(loggerName, name, enableFileOutput) {
+    const Logger = require('./lib/loggers/' + loggerName);
     var logger = new Logger(name, enableFileOutput);
+    module.exports = logger;
     return logger;
 }
--- test.js
+++ test.js
@@ -1,6 +1,20 @@
-const SuperLogger = require('./index');
+var log = require('./index').setup('bunyan', 'my-project', true);
 
-var log = SuperLogger.bunyan('kukareku', true);
-log.level('silly');
-log.silly('kukareku, %s = %s', 'mimi' , 11, {pipi: { iii: 333}});
-log.silly('kukareku, %s = %s', 'mimi' , 11, {pipi: { iii: 333}}, {yyy: { zzz: 22}});
+
+log = require('./index');
+
+log.level('verbose');
+
+log.info('Hello from the app');
+log.error('Something went wrong. reason: %s', 'Object is null', {userId: 11, email: 'john@doe.com'});
+log.warn('Objects are not same. ',{userId: 11, email: 'john@doe.com'}, {userId: 22, email: 'jane@doe.com'});
+
+try {
+    throw new Error('Index is out of range');
+} catch (e) {
+    log.exception(e);
+    log.error(e);
+    log.error('There was en exception', e);
+} finally {
+    log.verbose('Processing completed. Elapsed time: %s sec.', 15);
+}
