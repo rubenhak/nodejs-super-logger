ada516bef66981e3141d2159630aa745daa62011
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-01-18 12:48:38 -0800

    changes to setup procedures

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const RootLogger = require('./lib/root');
 
-exports.setup = function(provider, name, options) {
-    const root = new RootLogger(provider, name, options);
-    module.exports = root.logger;
-    return root.logger;
+function createGlobal(provider, name, options) {
+    module._globalProvider = provider;
+    module._globalOptions = options;
+
+    var rootLogger = new RootLogger(provider, name, options);
+
+    module.exports = rootLogger.logger;
+    module.exports.setup = createLocal;
+
+    return module.exports;
 }
+
+function createLocal(provider, name, options) {
+    if (module._globalProvider) {
+        provider = module._globalProvider;
+    }
+    if (module._globalOptions) {
+        options = module._globalOptions;
+    }
+    var logger = new RootLogger(provider, name, options);
+    return logger;
+}
+
+module.exports.setup = createGlobal;
