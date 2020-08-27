5ce496ba66b5bc1c3e21ee63c8a927cd53e3fd82
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-08-26 21:33:55 -0700

    working on outputStream creator

--- src/base.ts
+++ src/base.ts
@@ -63,6 +63,70 @@ class BaseLogger
     {
 
     }
+
+
+
+    outputStream(fileName: string)
+    {
+        if (!this.options.enableFile) {
+            return null;
+        }
+
+        var filePath = pathJoin(this._root.rootDir!, fileName);
+        var writer = createWriteStream(filePath);
+
+        var wrapper = {
+            _indent: 0,
+            write: (str) => {
+                if (_.isNotNullOrUndefined(str)) {
+                    if (_.isObject(str)) {
+                        for (var x of JSON.stringify(str, null, 4).split('\n'))
+                        {
+                            writer.write('    '.repeat(wrapper._indent));
+                            writer.write(x);
+                            writer.write('\n');
+                        }
+                    } else {
+                        writer.write('    '.repeat(wrapper._indent));
+                        writer.write(str);
+                        writer.write('\n');
+                    }
+                }
+            },
+            writeHeader: (str) => {
+                wrapper.write();
+                wrapper.write('**** ' + str);
+            },
+            indent: () => {
+                wrapper._indent++;
+            },
+            unindent: () => {
+                wrapper._indent--;
+            },
+            close: () => {
+                return new Promise((resolve, reject) => {
+                    writer.on('error', (err) => {
+                        reject(err);
+                    });
+                    writer.end(null, null, () => {
+                        resolve();
+                    });
+                    writer.end();
+                });
+            }
+        }
+        return wrapper;
+    }
+
+    outputFile(fileName, contents)
+    {
+        var writer = this.outputStream(fileName);
+        if (!writer) {
+            return Promise.resolve();
+        }
+        writer.write(contents);
+        return writer.close();
+    }
 }
 
 export { BaseLogger };
